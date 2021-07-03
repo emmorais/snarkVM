f4418cab3eb8b08c6b60a5028c2835873a532fc4
emmorais <eduardo.morais@gmail.com>
2021-07-03 14:29:17 -0300

    Implement hash to curve for BLS12_377 g2

--- algorithms/src/hash_to_curve/hash_to_curve.rs
+++ algorithms/src/hash_to_curve/hash_to_curve.rs
@@ -16,34 +16,30 @@
 
 use crate::crh::sha256::sha256;
 use rand_chacha::{rand_core::SeedableRng, ChaCha8Rng};
-use snarkvm_curves::{
-    bls12_377::{Fq, G1Affine},
-    AffineCurve,
-};
-use snarkvm_fields::{One, Zero};
+use snarkvm_curves::{bls12_377::{Fq, G1Affine}, AffineCurve};
+use snarkvm_fields::{Field, Fp2};
+use snarkvm_curves::bls12_377::{G2Affine, Fq2Parameters};
 use snarkvm_utilities::UniformRand;
-use std::ops::AddAssign;
 
-pub fn hash_to_curve(input: &str) -> G1Affine {
+pub fn hash_to_curve<G: AffineCurve<BaseField = F>, F: Field>(input: &str) -> G {
     let mut rng = ChaCha8Rng::from_seed(sha256(input.as_bytes()));
-    let mut x = Fq::rand(&mut rng);
+    let mut x = G::BaseField::rand(&mut rng);
     loop {
         for bit_y in [false, true] {
-            if let Some(g1_proj) = G1Affine::from_x_coordinate(x.clone(), bit_y) {
-                let g1_proj = g1_proj.scale_by_cofactor();
-                if !g1_proj.is_zero() {
-                    let g1 = G1Affine::from(g1_proj);
-                    return g1;
+            if let Some(g) = G::from_x_coordinate(x.clone(), bit_y) {
+                let g = g.mul_by_cofactor();
+                if !g.is_zero() {
+                    return g;
                 }
             }
         }
-        x.add_assign(&Fq::one());
+        x.add_assign(&F::one());
     }
 }
 
 #[test]
-fn hash_bls12_377() {
-    let g1 = hash_to_curve("Aleo BLS12-377 G1");
+fn hash_bls12_377_g1() {
+    let g1 = hash_to_curve::<G1Affine, Fq>("Aleo BLS12-377 G1");
     assert!(g1.is_in_correct_subgroup_assuming_on_curve());
     assert_eq!(
         g1.x.to_string(),
@@ -52,5 +48,19 @@ fn hash_bls12_377() {
     assert_eq!(
         g1.y.to_string(),
         "47988536770352052135994920271989708481287122349204081233627486611634010967671014168936729210354715817984213346861761"
+    );
+}
+
+#[test]
+fn hash_bls12_377_g2() {
+    let g2 = hash_to_curve::<G2Affine, Fp2<Fq2Parameters>>("Aleo BLS12-377 G2");
+    assert!(g2.is_in_correct_subgroup_assuming_on_curve());
+    assert_eq!(
+        g2.x.to_string(),
+        "Fp2(4992399534085505417963743334831158275796011776806012206913627929721152622217262086900539913823010591400808061223857 + 1036762444117213072465519435695624108541353692644436531612240142717350874959576322311161822617113219647751577789565 * u)"
+    );
+    assert_eq!(
+        g2.y.to_string(),
+        "Fp2(39593503541928207112672510593382719635487710939976849056149175312270166418672211148604934770065425690518397740619 + 89614077544256851417964173631152027483300723040827568531499279961411912919170578736548531738044390218650282162770 * u)"
     );
 }
